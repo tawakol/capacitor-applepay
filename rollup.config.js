de1899f78ebaa17d550e54437611b2e07a6e2445
Halim <halimsamy6@gmail.com>
2023-11-09 19:08:06 +0200

    Fix capitalization of CapacitorApplepay in podspec and rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorApplePay',
+      name: 'CapacitorApplepay',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
